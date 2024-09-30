1b32f9b9cbd69768a58c5e2f9a997762210b4634
bimbims125 <pramvendread@gmail.com>
2024-10-01 01:02:33 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const port = process.env.PORT
+const app = express()
+
+app.get('/', async (req, res)=>{
+  res.send(
+    {
+      message: 'Success'
+    }
+  )
+})
+
+app.listen(process.env.PORT, ()=>{
+  console.log(`Success ${port}`)
+})
