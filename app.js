acdfcae0346fbb25b1f58ca670cc6b254a7ed7b9
bimbims125 <pramvendread@gmail.com>
2024-10-02 10:41:41 +0700

    Change

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const express = require('express')
 const port = process.env.PORT
 const app = express()
 
-app.get('/', async (req, res)=>{
-  res.send(
-    {
-      message: 'Success test webhook'
+const { exec } = require('child_process');
+
+app.get('/', async (req, res) => {
+  exec('whoami', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send({ message: 'Error executing command' });
     }
-  )
-})
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+    }
+
+    res.send({
+      message: 'Success test webhook',
+      user: stdout.trim()
+    });
+  });
+});
 app.get('/test-webhook', async (req, res)=>{
   res.send(
     {
